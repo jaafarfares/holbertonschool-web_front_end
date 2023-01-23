f3fd2ba7f2542eb5d53d886afe0732d2122421e3
jaafar <jaafarferes@gmail.com>
2023-01-23 12:35:01 +0100

    python annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function add that takes
+a float a and a float b as arguments and
+returns their sum as a float."""
+
+
+def add(a: float, b: float) -> float:
+    """a function that add 'a' and 'b' and
+    returns their sum as a float """
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Write a type-annotated function floor which
+takes a float n as argument
+and returns the floor of the float. """
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """ takes a float n as argument and returns the floor of the float."""
+    return math.floor(n)
